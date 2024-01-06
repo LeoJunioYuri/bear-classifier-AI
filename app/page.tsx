99aef13b0b3238b86194390b0e5f64eb3aec5198
leojunioyuri <leojunioyuri@hotmail.com>
2024-01-06 04:30:12 -0300

    fix: version 1.0

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import React, { useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 
+interface ImageInputProps {
+  onImageChange: (image: tf.PixelData | ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement | ImageBitmap) => void;
+}
+
 // Um componente que renderiza um botão de input da imagem e um elemento de imagem
-const ImageInput = ({ onImageChange }) => {
+const ImageInput: React.FC<ImageInputProps> = ({ onImageChange }) => {
   const [image, setImage] = useState(null);
 
   // Criar uma referência do React
@@ -52,14 +56,15 @@ const ClassificationResult = ({ result }) => {
     // Retornar um elemento de texto com a classe e a confiança
     return (
       <p>
-        A imagem é um(a) <b>{className}</b> com <b>{percentage}%</b> de confiança.
+        A imagem é um(a) <b>{className ?? "desconhecido"}</b> com <b>{percentage ?? "0"}%</b> de confiança.
       </p>
     );
   } else {
-    // Retornar um elemento de texto vazio
-    return <p></p>;
+    // Retornar um elemento de texto com uma mensagem de erro
+    return <p>Não foi possível classificar a imagem.</p>;
   }
 };
+
 
 // O componente principal da página
 const Home = () => {
@@ -82,14 +87,20 @@ const Home = () => {
   const classifyImage = async (image: tf.PixelData | ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement | ImageBitmap) => {
     // Converter a imagem em um tensor
     const tensor = tf.browser.fromPixels(image);
-    // Classificar o tensor usando o modelo
-    const predictions = await model.classify(tensor);
-    // Obter a primeira predição
-    const result = predictions[0];
-    // Atualizar o estado com o resultado
-    setResult(result);
-    // Mostrar o resultado no console
-    console.log(result);
+    // Verificar se o modelo existe
+    if (model) {
+      // Classificar o tensor usando o modelo
+      const predictions = await model?.classify(tensor);
+      // Obter a primeira predição
+      const result = predictions[0];
+      // Atualizar o estado com o resultado
+      setResult(result);
+      // Mostrar o resultado no console
+      console.log(result);
+    } else {
+      // Mostrar uma mensagem de erro no console
+      console.error("O modelo ainda não foi carregado");
+    }
   };
 
   // Uma função que é chamada quando a página é montada
