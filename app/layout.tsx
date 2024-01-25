705516aa035477d2a019b54f55ca4646fc31f769
leojunioyuri <leojunioyuri@hotmail.com>
2024-01-25 16:05:24 -0300

    fix: rollback layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,21 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Bear and Teddy Bear Classifier',
   description: 'An intelligent image classifier for distinguishing between various bear species and teddy bears. Powered by machine learning, this web app uses advanced algorithms to analyze and categorize uploaded images with high accuracy.',
 };
 
-
-export function BodyLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <body className={inter.className}>
-      {children}
-    </body>
-  );
-}
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <BodyLayout>{children}</BodyLayout>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
